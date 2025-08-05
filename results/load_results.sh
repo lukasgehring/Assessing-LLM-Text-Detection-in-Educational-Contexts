44641ec6e02b6e8f7abc36c17187831e34ff8e01
Lukas Gehring <lukas.gehring@uni-bielefeld.de>
2025-08-05 10:03:21 +0200

    remove logs and results

--- results/load_results.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Loading results..."
-rsync -avP citec-cluster:projects/BenchEduLLMDetect/results/ .
-
-echo "Loading logs..."
-rsync -avP citec-cluster:projects/BenchEduLLMDetect/logs/ ../logs/
--- results/load_results_dry_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Loading results..."
-rsync -avP citec-cluster:projects/BenchEduLLMDetect/results/ . --dry-run
-
-echo "Loading logs..."
-rsync -avP citec-cluster:projects/BenchEduLLMDetect/logs/ ../logs/ --dry-run
